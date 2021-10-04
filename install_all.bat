ea268723197cea3ebcb28495f62bcbb2a13c1a49
TijmenSimons <70627990+TijmenSimons@users.noreply.github.com>
2021-10-04 20:24:34 +0200

    Working installation file

--- install_all.bat
+++ install_all.bat
@@ -51,6 +51,23 @@ goto:lastPart
 
 :otherError
 echo uhh... there seems to be an error :c please contact an administrator.
+
+SET /P AREYOUSURE=Did you get an error saying somthing about 'Microsoft Visual C++ 14.0 is required... etc' ( Y / N )?
+IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
+
+echo. 2>c++_error_help_file.txt
+echo Welcome, it seems you got this error.>> c++_error_help_file.txt
+echo --> Downloaded Microsoft Visual C++ Build Tools from this link: https://visualstudio.microsoft.com/downloads/>> c++_error_help_file.txt
+echo --> Run the installer>> c++_error_help_file.txt
+echo --> Select: Workloads → Visual C++ build tools.>> c++_error_help_file.txt
+echo --> Install options: select only the “Windows 10 SDK” (assuming the computer is Windows 10)>> c++_error_help_file.txt
+echo A file has been created for you called 'c++_error_help_file'.
+echo Maybe there you can find a solution.
+echo.
+PAUSE
+
+:END
+
 PAUSE
 
 goto:eof
