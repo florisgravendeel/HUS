b919bc4afecebf855461112e4517086b1734967c
TijmenSimons <70627990+TijmenSimons@users.noreply.github.com>
2021-10-05 10:11:47 +0200

    Made help file better

--- x_install_all.bat
+++ x_install_all.bat
@@ -80,10 +80,10 @@ IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
 echo. 2>c++_error_help_file.txt
 echo Welcome, it seems you got this error.>> c++_error_help_file.txt
-echo --> Downloaded Microsoft Visual C++ Build Tools from this link: https://visualstudio.microsoft.com/downloads/#build-tools-for-visual-studio-2019>> c++_error_help_file.txt
-echo --> Run the installer>> c++_error_help_file.txt
-echo --> Select: Workloads → Visual C++ build tools.>> c++_error_help_file.txt
-echo --> Install options: select only the “Windows 10 SDK” (assuming the computer is Windows 10)>> c++_error_help_file.txt
+echo -- Downloaded Microsoft Visual C++ Build Tools from this link: https://visualstudio.microsoft.com/downloads/#build-tools-for-visual-studio-2019>> c++_error_help_file.txt
+echo -- Run the installer>> c++_error_help_file.txt
+echo -- Select: Workloads → Visual C++ build tools.>> c++_error_help_file.txt
+echo -- Install options: select only the “Windows 10 SDK” (assuming the computer is Windows 10)>> c++_error_help_file.txt
 echo A file has been created for you called 'c++_error_help_file'.
 echo Maybe there you can find a solution.
 echo.
