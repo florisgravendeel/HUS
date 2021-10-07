6eab16ff44c9a6f5cd0f539f47d8d89f50be6119
Diego Rolf <djdiegorof@gmail.com>
2021-10-07 14:20:38 +0200

    De omgeving beter gemaakt
    Ik heb de omgeving van de front-end wat duidelijker gemaakt om in te werken. Verder zitten er nu een paar voorbeelden bij waarmee gewerkt kan worden.

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -6,9 +6,26 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 
 #--------------------------------------------------------------------------------------------DEPENDENCIES
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="../templates")
+app.mount("/static", StaticFiles(directory="../static"), name="static")
 #--------------------------------------------------------------------------------------------ROUTES
 
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("homepage.html", {"request": request})
+    data = {
+        1: {
+            "link": "google.com",
+            "text": "Link to Google"
+        },
+        2: {
+            "link": "bing.com",
+            "text": "Link to Bing"
+        }
+    }
+    return templates.TemplateResponse("homepage.html", {"request": request, "data": data})
+
+@app.get("/login", response_class=HTMLResponse)
+async def login(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+
