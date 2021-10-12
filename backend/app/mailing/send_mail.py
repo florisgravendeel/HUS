64e51c586f47ef13930dcf0133703d5fb23ff14f
TijmenSimons <70627990+TijmenSimons@users.noreply.github.com>
2021-10-12 09:18:52 +0200

    setting up the files

---
+++ backend/app/mailing/send_mail.py
@@ -0,0 +1,9 @@
+from fastapi import FastAPI, BackgroundTasks, UploadFile, File, Form
+from starlette.responses import JSONResponse
+from starlette.requests import Request
+from fastapi_mail import FastMail, MessageSchema,ConnectionConfig
+from pydantic import BaseModel, EmailStr
+from typing import List
+
+def function():
+    print('pog')
