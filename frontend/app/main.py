1b6ca2c31bcd74aa0edba65477fd3a04e4742b67
segervvv <seger.vanvlijmen@student.hu.nl>
2021-10-14 09:30:40 +0200

    Templates voor templates

--- frontend/app/main.py
+++ frontend/app/main.py
@@ -11,6 +11,23 @@ templates = Jinja2Templates(directory="../templates")
 app.mount("/static", StaticFiles(directory="../static"), name="static")
 #--------------------------------------------------------------------------------------------ROUTES
 
+class customer:
+    def __init__(self, href, caption):
+        self.href = href
+        self.caption = caption
+
+boi = [customer('http://localhost:8080/item/', 'the best website'), customer('base.html', 'pagina')]
+
 @app.get("/", response_class=HTMLResponse)
 async def home(request: Request):
-    return templates.TemplateResponse("main.html", {"request": request})
+    return templates.TemplateResponse("include/bovenBalk.html", {"request": request, "customer":boi})
+
+
+@app.get("/item/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("base.tml", {"request": request})
+
+
+@app.get("/test/", response_class=HTMLResponse)
+async def home(request: Request):
+    return templates.TemplateResponse("bovenBalk.html", {"request": request})
