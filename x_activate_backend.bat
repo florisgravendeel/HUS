ad01c4530abef515252c6a2bfa4ee07b522ca3b3
TijmenSimons <70627990+TijmenSimons@users.noreply.github.com>
2021-11-01 09:13:05 +0100

    commit backed console batch file

--- x_activate_backend.bat
+++ x_activate_backend.bat
@@ -6,9 +6,9 @@ echo.
 echo If you wish to exit, it would be nice of you to press 'CTRL + C'
 echo You can also use 'CTRL + C' to reboot the server and entering 'N'.
 echo.
-pause
-venv\scripts\activate.bat
-pause
+
+virtualenv venv
+
 cd .\backend\app
 :run
 python main.py
